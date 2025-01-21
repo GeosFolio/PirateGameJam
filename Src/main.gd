9bc30688d7d6f5e4d8977f90fc57bdbbe45b4a03
nhilayne <nhilayne@gmail.com>
2025-01-21 02:30:02 -0800

    Button calls to adjust resources, ending checks

--- Src/main.gd
+++ Src/main.gd
@@ -1,1 +1,10 @@
 extends Node
+
+func _on_yes_button_pressed():
+	$GameManager.handle_choice("accept")
+
+func _on_no_button_pressed():
+	$GameManager.handle_choice("deny")
+
+func _on_nom_button_pressed():
+	$GameManager.handle_choice("nom")
